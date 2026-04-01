be0e19c23c62b03cb5b8a3d2516f14d49741bf25
Neil <bn0010100@gmail.com>
2026-04-01 18:08:31 +0800

    Add ei version command

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "github.com/tta-lab/einai/cmd"
 
+var (
+	version   = "dev"
+	buildDate = "unknown"
+	goVersion = "unknown"
+)
+
 func main() {
+	cmd.SetBuildInfo(version, buildDate, goVersion)
 	cmd.Execute()
 }
